7b70e347d7acbeff5ccba43bed41d8fbd8b76244
hclement <clement.herbin@hotmail.com>
2024-05-02 13:52:40 +0200

    update create

--- front/src/Component/create/Create.tsx
+++ front/src/Component/create/Create.tsx
@@ -5,24 +5,29 @@ import CreateQuestion from './CreateQuestion';
 
 const Line = () => {
     return (
-        <div className="line"/>
+        <div className="line" />
     )
 }
 
 const Create = () => {
     return (
         <div className="home">
-            <Header/>
-            <div className="create-content">
-                <div className="create-title">
-                    <div className="create-input">
-                        <div>Quizz title</div>
-                        <input type="text" placeholder="Quizz"/>
+            <Header />
+            <div className="create">
+                <div className="create-content">
+                    <div className="create-title">
+                        <div className="create-input">
+                            <div>Quizz title</div>
+                            <input type="text" placeholder="Quizz" />
+                        </div>
+                    </div>
+                    <Line />
+                    <CreateQuestion />
+                    <Line />
+                    <div className="create-input test-question">
+                        <button className="quizz-button-add-question">Add question</button>
                     </div>
                 </div>
-                <Line/>
-                <CreateQuestion/>
-                <button className="quizz-add-button">Add</button>
             </div>
         </div>
     )
--- front/src/Component/create/CreateQuestion.tsx
+++ front/src/Component/create/CreateQuestion.tsx
@@ -1,11 +1,25 @@
+import React from 'react';
 import './create.css';
 
 import Answer from './CreateAnswer';
 
+const Question = (id: number): React.JSX.Element => {
+    return (
+        <div className="create-input">
+            <div>Question {id}</div>
+            <input className="create-input-question" type="text" placeholder="Question" />
+        </div>
+    )
+}
+
 const CreateQuestion = () => {
     return (
         <div className="create-question">
-            <Answer/>
+            {Question(1)}
+            <Answer />
+            <div className="create-input">
+                <button className="quizz-button-add-answer">Add answer</button>
+            </div>
         </div>
     )
 }
