d382842bdf7b92cb150acc7fbe33fc9bb56f547b
hclement <clement.herbin@hotmail.com>
2024-05-02 11:33:04 +0200

    update login & register

--- front/src/Component/login/Login.tsx
+++ front/src/Component/login/Login.tsx
@@ -1,33 +1,51 @@
-import './login.css';
-
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Login = ({}) => {
+import './login.css';
 
-  return (
-    <div className="login">
-        <div className="left">
-            
-        </div>
-        <div className="right">
-            <div className="login-form">
-                <div className="login-form-title">Login</div>
-                <div className="login-form-entry">
-                    <div className="name">Username</div>
-                    <input type="text" placeholder="Username"/>
-                </div>
-                <div className="login-form-entry">
-                    <div className="name">Password</div>
-                    <input type="password" placeholder="Password"/>
-                </div>
-                <button type="button">Login</button>
+const Login = () => {
+
+    const [username, setUsername] = useState<string>("");
+    const [pwd, setPwd] = useState<string>("");
+
+    const updateUsername = (e: React.FormEvent<HTMLInputElement>): void => {
+        setUsername(e.currentTarget.value);
+    }
+
+    const updatePwd = (e: React.FormEvent<HTMLInputElement>): void => {
+        setPwd(e.currentTarget.value);
+    }
+
+    const handleClick = (): void => {
+        if (username !== "" && pwd !== "") {
+            console.log("Click !");
+        }
+    }
+
+    return (
+        <div className="login">
+            <div className="left">
+
             </div>
-            <Link to="/register">
-                <div className="toRegister">Vous n'avez pas de compte ?</div>
-            </Link>
+            <div className="right">
+                <div className="login-form">
+                    <div className="login-form-title">Login</div>
+                    <div className="login-form-entry">
+                        <div className="name">Username</div>
+                        <input type="text" placeholder="Username" value={username} onChange={e => updateUsername(e)} />
+                    </div>
+                    <div className="login-form-entry">
+                        <div className="name">Password</div>
+                        <input type="password" placeholder="Password" value={pwd} onChange={e => updatePwd(e)} />
+                    </div>
+                    <button type="button" onClick={() => handleClick()}>Login</button>
+                </div>
+                <Link to="/register">
+                    <div className="toRegister">Vous n'avez pas de compte ?</div>
+                </Link>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Login;
--- front/src/Component/register/Register.tsx
+++ front/src/Component/register/Register.tsx
@@ -1,26 +1,45 @@
-import './register.css';
-
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Register = ({}) => {
+import './register.css';
+
+
+const Register = ({ }) => {
+
+  const [username, setUsername] = useState<string>("");
+  const [pwd, setPwd] = useState<string>("");
+
+  const updateUsername = (e: React.FormEvent<HTMLInputElement>): void => {
+    setUsername(e.currentTarget.value);
+  }
+
+  const updatePwd = (e: React.FormEvent<HTMLInputElement>): void => {
+    setPwd(e.currentTarget.value);
+  }
+
+  const handleClick = (): void => {
+    if (username !== "" && pwd !== "") {
+      console.log("Click !");
+    }
+  }
 
   return (
     <div className="register">
-        <div className="register-form">
-            <div className="register-form-title">Register</div>
-            <div className="register-form-entry">
-                <div className="name">Username</div>
-                <input type="test" placeholder="Username"/>
-            </div>
-            <div className="register-form-entry">
-                <div className="name">Password</div>
-                <input type="password" placeholder="Password"/>
-            </div>
-            <button type="button">Register</button>
+      <div className="register-form">
+        <div className="register-form-title">Register</div>
+        <div className="register-form-entry">
+          <div className="name">Username</div>
+          <input type="test" placeholder="Username" value={username} onChange={e => updateUsername(e)} />
         </div>
-        <Link to="/login">
-          <div className="toLogin">Vous avez déjà un compte ?</div>
-        </Link>
+        <div className="register-form-entry">
+          <div className="name">Password</div>
+          <input type="password" placeholder="Password" value={pwd} onChange={e => updatePwd(e)} />
+        </div>
+        <button type="button" onClick={() => handleClick()}>Register</button>
+      </div>
+      <Link to="/login">
+        <div className="toLogin">Vous avez déjà un compte ?</div>
+      </Link>
     </div>
   );
 }
