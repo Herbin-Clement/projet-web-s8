9b8788630b0f6bf4b7359d6bd56e99f2969c1a92
rcrico <ruben.crico@etu.inp-n7.fr>
2024-05-04 22:01:33 +0200

    debut facade et user

--- Projet-ApplicationWeb/src/main/java/BackEnd/Facade.java
+++ Projet-ApplicationWeb/src/main/java/BackEnd/Facade.java
@@ -1,5 +1,53 @@
 package BackEnd;
+import javax.ejb.*;
+import javax.persistence.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import java.util.Collection;
+import java.util.List;
+
+@Singleton
+@Path("/rest")	// TODO: à configurer
 public class Facade {
+
+	 @PersistenceContext
+     EntityManager em;
+
+	 private boolean UsernameNotUsed(String username) {
+		 String sqlQuery = "SELECT u FROM User u";
+	     Collection<User> users = this.em.createQuery(sqlQuery, User.class).getResultList();
+	     Boolean notUsed = true;
+		 for (User user : users) {
+			 if (user.getUsername().equals(username))  {
+				 notUsed = false;
+			 }
+		 }
+		 return notUsed;
+	 }
+	 
+	 @POST
+	 @Consumes(MediaType.APPLICATION_JSON) //TODO : bon ?
+	 public void addUser(String username, String password) {
+		 if (UsernameNotUsed(username)) {
+			 User user = new User(username, password);
+			 em.persist(user);
+			 return Response.status(Response.Status.CREATED).build();
+		 } else {
+			 return Response.status(Response.Status.BAD_REQUEST).entity("Username already used.").build();
+		 }
+
+	 }
+	 
+	 /*{
+	 public void ajoutQuizz(Collection<Mcq> liste_qcm, String link, int id_user_createur)
+	 majStats(int id_quizz)
+	 ajoutReponsesQuizz(Collection<Reponse> reponses, int id_quizz)
+	 listeQuizzs()
+	 listeUsers()
+	 associerCreateur(int id_quizz, int id_user)
+	 getStatistiquesSondage(int id_sondage)
+	 }*/
 
 }
--- Projet-ApplicationWeb/src/main/java/BackEnd/User.java
+++ Projet-ApplicationWeb/src/main/java/BackEnd/User.java
@@ -1,5 +1,55 @@
 package BackEnd;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.persistence.*;
+
+@Entity
 public class User {
 
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+	private String username;
+	private String password;
+	
+	@OneToMany(mappedBy="creator", fetch = FetchType.EAGER)
+	Collection<Quizz> createdQuizzes;
+	@ManyToMany(mappedBy="participant", fetch = FetchType.EAGER) // TODO : bon ?
+	Collection<Quizz> answeredQuizzes;
+	
+	public User() {};
+	
+	public User(String username, String password) {
+		this.setUsername(username);
+		this.setPassword(password);
+	   	this.createdQuizzes = new LinkedList<Quizz>();
+	   	this.answeredQuizzes = new LinkedList<Quizz>(); // TODO : bon ?
+	}
+
+	public Collection<Quizz> getCreatedQuizzes() {
+		return this.createdQuizzes;
+	}
+	
+	public Collection<Quizz> getAnsweredQuizzes() {
+		return this.answeredQuizzes;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
