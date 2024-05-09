d5985881a925126244c22d9924ea8dbcb700e833
rcrico <ruben.crico@etu.inp-n7.fr>
2024-05-09 13:44:34 +0200

    maj

--- Projet-ApplicationWeb/src/main/java/BackEnd/RestApp.java
+++ Projet-ApplicationWeb/src/main/java/BackEnd/RestApp.java
@@ -3,8 +3,10 @@ package BackEnd;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@ApplicationPath("/rest")
 public class RestApp extends Application {
 	
 	private Set<Object> singletons = new HashSet<Object>();
