4a6f5d169111b35c349195b89fde57afa12f1ff9
rcrico <ruben.crico@etu.inp-n7.fr>
2024-05-11 19:33:36 +0200

    server.war OK; pas reussi postman

---
+++ back/MavenWebApp/src/main/java/pack/Input.java
@@ -0,0 +1,15 @@
+package pack;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Input {
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+}
